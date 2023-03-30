ba9ed932c5aba02fc2b5e393d4bb6b8b41e8fdb4
Warren Dubery <warrendubery@protonmail.ch>
2023-03-30 10:29:05 +0100

    relocate portfolio data

---
+++ src/data/portfolio-data.js
@@ -0,0 +1,33 @@
+import IMG1 from "../assets/portfolio1.jpg";
+import IMG2 from "../assets/portfolio2.jpg";
+import IMG3 from "../assets/portfolio3.jpg";
+
+export const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Mars DAO",
+    blurb:
+      "An MVP frontend to Mars DAO (Decentralised Autonomous Organisation). DAOs are made possible through blockchain technology. This project is a proof-of-concept and currently uses a traditional backend for data saving and retrieval. ",
+    github: "https://github.com/wardu/Mars-DAO",
+    demo: "https://mars-dao-wardu.vercel.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Blackjack Game",
+    blurb:
+      "An implementation of the classic casino game Blackjack. This project was built using JQuery. The game is playable on desktop and mobile devices.",
+    github: "https://github.com/wardu/blackjack",
+    demo: "https://wardu.github.io/blackjack/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "In Stock Warehouse App",
+    blurb:
+      "Built in collaboration, this app is designed to help warehouse managers keep track of stock levels. The app is built within the MVP design framework.",
+    github: "https://github.com/wardu/inStock",
+    demo: "#",
+  },
+];
